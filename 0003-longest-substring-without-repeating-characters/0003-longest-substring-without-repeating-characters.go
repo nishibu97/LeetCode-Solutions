82a27327513cdbae8c9518280aa5c7ccefe9ad3f
Kohei Nishibu <108510992+nishibu97@users.noreply.github.com>
2026-07-14 22:37:41 +0900

    Time: 4 ms (69.49%), Space: 4.9 MB (66.96%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
@@ -0,0 +1,22 @@
+func lengthOfLongestSubstring(s string) int {
+    lastSeen := make(map[byte]int)
+    left := 0
+    maxLength := 0
+
+    for right := 0; right < len(s); right++ {
+        char := s[right]
+
+        if index, found := lastSeen[char]; found && index >= left {
+            left = index + 1
+        }
+
+        lastSeen[char] = right
+
+        length := right - left + 1
+        if length > maxLength {
+         maxLength = length
+        }
+    }
+
+    return maxLength
+}
