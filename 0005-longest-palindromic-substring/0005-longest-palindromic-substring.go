6bebe791a6a4f26f603f838490c71907458587db
Kohei Nishibu <108510992+nishibu97@users.noreply.github.com>
2026-07-16 17:55:33 +0900

    Time: 3 ms (55.88%), Space: 4.4 MB (82.63%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -0,0 +1,31 @@
+// 回文を探す問題。
+func longestPalindrome(s string) string {
+    if len(s) == 0 {
+        return ""
+    }
+
+    start, end := 0,0
+    for i := 0; i < len(s); i++ {
+        // 奇数長: "aba"
+		l1, r1 := expandAroundCenter(s, i, i)
+		// 偶数長: "abba"
+		l2, r2 := expandAroundCenter(s, i, i+1)
+
+        if r1-l1 > end-start {
+            start, end = l1, r1
+        }
+        if r2-l2 > end-start {
+            start, end = l2, r2
+        }
+    }
+    return s[start: end+1]
+}
+
+// 中央から左右にポインタを移動し探索
+func expandAroundCenter(s string, left, right int) (int, int) {
+    for left >= 0 && right < len(s) && s[left] == s[right] {
+        left--
+        right++
+    }
+    return left + 1, right - 1
+}
