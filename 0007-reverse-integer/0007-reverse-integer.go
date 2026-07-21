0169c91123a096d47ff2f1196f0096ff6fc3e9ed
Kohei Nishibu <108510992+nishibu97@users.noreply.github.com>
2026-07-21 18:37:29 +0900

    Time: 5 ms (20.47%), Space: 3.9 MB (61.13%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.go
@@ -0,0 +1,18 @@
+func reverse(x int) int {
+    const IntMax = 1<<31 - 1
+    const IntMin = -1 << 31
+
+    rev := 0
+    for x != 0 {
+        pop := x % 10
+        x /= 10
+        if rev > IntMax/10 || (rev == IntMax/10 && pop > 7) {
+            return 0
+        }
+        if rev < IntMin/10 || (rev == IntMin/10 && pop < -8) {
+            return 0
+        }
+        rev = rev*10 + pop
+    }
+    return rev
+}
